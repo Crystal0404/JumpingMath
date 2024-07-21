2fdfcc1f9661fab9ec9a9f48bba9cef0edc7e0ea
Crystal0404 <syh040325@163.com>
2024-07-21 17:39:20 +0800

    jitpack

--- src/main/java/crystal0404/Main.java
+++
@@ -1,9 +0,0 @@
-package crystal0404;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(JPMath.godFormula("主"));
-        System.out.println(JPMath.jpPow(2, 3));
-        System.out.println(JPMath.jpSigma("Σ"));
-    }
-}
